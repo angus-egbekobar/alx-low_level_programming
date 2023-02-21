bb345b8db935db69042e3234be660ec0225aedad
angus-egbekobar <angusegbekobar@gmail.com>
2023-02-21 08:09:07 -0800

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -10,19 +10,16 @@
 
 void print_alphabet(void)
 {
-	char i;
+	char i = 'a';
 
-	for (i = 'a'; i <= 'z'; i++)
+	while (i <= 'z')
 	{
 		_putchar(i);
-
-
-
-
+	i++;
 
 
 	}
-
+	_putchar('\n');
 
 
 }
