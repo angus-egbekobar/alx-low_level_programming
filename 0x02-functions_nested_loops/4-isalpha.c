40145e66833afd1c38a622e916a2c930026bc338
angus-egbekobar <angusegbekobar@gmail.com>
2023-02-21 13:38:55 -0800

    5-sign.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _isalpha - function  that checks for alphabet
+ *
+ * @c: to return a parameter
+ *
+ * Return:  if c is a letter, lowercase or uppercase
+ * and 0 otherwise
+ *
+ */
+
+
+int _isalpha(int c)
+{
+	if((c >= 97 &&c <= 122) || (c <= &&c <= 90))
+		return (i);
+
+	else
+
+		return (0);
+
+
+
+
+
+
+}
