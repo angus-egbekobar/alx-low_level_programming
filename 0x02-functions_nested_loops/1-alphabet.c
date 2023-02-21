ea42fe92156bd96e4af5f85af66accd43f43d811
angus-egbekobar <angusegbekobar@gmail.com>
2023-02-21 07:31:52 -0800

    1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_alphabet - a function that prints alphabet a to z in lower case
+ *
+ * Return: always 0
+ *
+ */
+
+
+void print_alphabet(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+
+	_putchar('\n');
+
+
+
+
+
+	}
+
+
+
+}
