df8fcb85e1df703e8cc28f6a0afe52fce4b4d1db
angus-egbekobar <angusegbekobar@gmail.com>
2023-02-21 14:17:00 -0800

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -13,20 +13,19 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		printf("+, ");
+		_putchar('+');
 		return (1);
-
 	}
 	else if (n == 0)
 	{
-		printf("0, ");
+		_putchar('0');
 		return (0);
 
 
 	}
 	else
 	{
-		printf("-, ");
+		_putchar('-');
 		return (-1);
 
 
@@ -38,3 +37,4 @@ int print_sign(int n)
 
 
 }
+
