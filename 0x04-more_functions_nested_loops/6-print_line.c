e132161b56cbff67fff01c895db22e3548cab220
angus-egbekobar <angusegbekobar@gmail.com>
2023-02-24 00:56:04 -0800

    6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,19 +1,21 @@
 #include "main.h"
 
 /**
- * print_line - print n underscore characters
- * @n: number of times to print _
+ * print_line - check for a digit
+ * @n : number of _ to be printed
+ * Return:void
  */
+
 void print_line(int n)
 {
-	int i;
 
-	if (n > 0)
+	int i = 0;
+
+	while (i < n && n > 0)
 	{
-		for (i = 0; i < n; i++)
-		{
-			_putchar('-');
-		}
+		_putchar('_');
+		i++;
 	}
 	_putchar('\n');
+
 }
