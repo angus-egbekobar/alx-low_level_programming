9b7f05c5d0a28c1cababf0701cdc94859ecf40ba
angus-egbekobar <angusegbekobar@gmail.com>
2023-03-06 19:30:34 -0800

    4-strpbrk.c

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -13,6 +13,7 @@ char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 char *leet(char *);
+char *_strpbrk(char *s, char *accept);
 
 #endif /* MAIN_H */
 
